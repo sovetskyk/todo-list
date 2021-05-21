540701515d6253312f7663e996a349d5365c105e
Ekaterina Savostina <74173325+sovetskyk@users.noreply.github.com>
2021-05-21 15:45:54 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const filterOption = document.querySelector('.filter-todo');
 document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('click', filterTodo);
+filterOption.addEventListener('change', filterTodo);
 
 //Functions
 function addTodo(e) {
